952f8c0783311d8a4add984f036ef3ee40d3f84a
webchroma <info@webchroma.de>
2024-04-22 11:39:59 +0000

    add views and cell paths to engine.rb

--- lib/decidim/feeds_ui/engine.rb
+++ lib/decidim/feeds_ui/engine.rb
@@ -7,6 +7,7 @@ module Decidim
   module FeedsUi
     # This is the engine that runs on the public interface of feeds_ui.
     class Engine < ::Rails::Engine
+      
       isolate_namespace Decidim::FeedsUi
 
       routes do
@@ -18,6 +19,12 @@ module Decidim
       initializer "FeedsUi.webpacker.assets_path" do
         Decidim.register_assets_path File.expand_path("app/packs", root)
       end
+
+      initializer "FeedsUi.add_cells_view_paths" do
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::FeedsUi::Engine.root}/app/cells")
+        Cell::ViewModel.view_paths << File.expand_path("#{Decidim::FeedsUi::Engine.root}/app/views")
+      end
+
     end
   end
 end
