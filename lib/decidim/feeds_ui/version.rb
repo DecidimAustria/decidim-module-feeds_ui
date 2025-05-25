82fa82621a5529f85969d23c1cfd281acea6eddd
Alex <alex@rusa.at>
2025-05-25 18:02:30 +0200

    Bump feeds and posts version numbers

--- lib/decidim/feeds_ui/version.rb
+++ lib/decidim/feeds_ui/version.rb
@@ -5,7 +5,7 @@ module Decidim
   module FeedsUi
     VERSION = "1.0.0"
     COMPAT_DECIDIM_VERSION = [">= 0.28.0", "< 0.29"].freeze
-    COMPAT_FEEDS_VERSION = [">= 1.0.0", "< 2"].freeze
-    COMPAT_POSTS_VERSION = [">= 1.0.0", "< 2"].freeze
+    COMPAT_FEEDS_VERSION = [">= 1.0.0", "< 4"].freeze
+    COMPAT_POSTS_VERSION = [">= 1.0.0", "< 3"].freeze
   end
 end
